474a36b6fdc8fc2ca8d23687cc13709fcee26644
Avinash Jat <dev.avinashjat@gmail.com>
2023-05-22 22:49:51 +0530

    create a js code of settimeout

---
+++ script.js
@@ -0,0 +1,24 @@
+// alert("hello birthday boy")
+
+// let a = setTimeout(function () {
+//     alert("hey ia m avi")
+  
+// },3000)
+
+// let b= prompt("do you want to continue")
+// if("n"==b){
+//     clearTimeout(a)
+//}
+
+
+const sum = (a,b,c) => {
+    console.log("yes i am running"+(a+b+c))
+    a+b+c
+}
+
+setTimeout(sum,1000,1,6,3)
+
+
+// setInterval(function(){
+//     alert("hjfverg,reagh")
+// },3000)
